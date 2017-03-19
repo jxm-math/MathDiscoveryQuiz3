d2395e76eb6a2fff84a005f871d9f0331eec6e39
jxm-math <juanjomadrigal326@gmail.com>
2017-03-19 17:26:27 +0100

    Feat: visibility of check marks + toggle functions

--- app/src/main/java/com/example/android/mathdiscoveryquiz3/MainActivity.java
+++ app/src/main/java/com/example/android/mathdiscoveryquiz3/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import static android.R.attr.visible;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,103 @@ public class MainActivity extends AppCompatActivity {
     boolean pict8 = false;
     boolean pict9 = false;
 
+    /* Toggles the visibility of the check mark 1*/
+    public void checkPicture1(View view) {
+        pict1 = !pict1;
+        ImageView checkImage = (ImageView) findViewById(R.id.check1);
+        if (pict1) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
 
+    /* Toggles the visibility of the check mark 2*/
+    public void checkPicture2(View view) {
+        pict2 = !pict2;
+        ImageView checkImage = (ImageView) findViewById(R.id.check2);
+        if (pict2) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 3*/
+    public void checkPicture3(View view) {
+        pict3 = !pict3;
+        ImageView checkImage = (ImageView) findViewById(R.id.check3);
+        if (pict3) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 4*/
+    public void checkPicture4(View view) {
+        pict4 = !pict4;
+        ImageView checkImage = (ImageView) findViewById(R.id.check4);
+        if (pict4) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 5*/
+    public void checkPicture5(View view) {
+        pict5 = !pict5;
+        ImageView checkImage = (ImageView) findViewById(R.id.check5);
+        if (pict5) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 6*/
+    public void checkPicture6(View view) {
+        pict6 = !pict6;
+        ImageView checkImage = (ImageView) findViewById(R.id.check6);
+        if (pict6) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 7*/
+    public void checkPicture7(View view) {
+        pict7 = !pict7;
+        ImageView checkImage = (ImageView) findViewById(R.id.check7);
+        if (pict7) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 8*/
+    public void checkPicture8(View view) {
+        pict8 = !pict8;
+        ImageView checkImage = (ImageView) findViewById(R.id.check8);
+        if (pict8) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /* Toggles the visibility of the check mark 9*/
+    public void checkPicture9(View view) {
+        pict9 = !pict9;
+        ImageView checkImage = (ImageView) findViewById(R.id.check9);
+        if (pict9) {
+            checkImage.setVisibility(View.VISIBLE);
+        } else {
+            checkImage.setVisibility(View.INVISIBLE);
+        }
+    }
 
 }
