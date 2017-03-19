547144216686fb8ff077b70017110ee65c73333f
jxm-math <juanjomadrigal326@gmail.com>
2017-03-19 10:08:08 +0100

    Feat: updated basic layout: grid layout + check picture

--- app/src/main/java/com/example/android/mathdiscoveryquiz3/MainActivity.java
+++ app/src/main/java/com/example/android/mathdiscoveryquiz3/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.mathdiscoveryquiz3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    boolean pict1 = false;
+    boolean pict2 = false;
+    boolean pict3 = false;
+    boolean pict4 = false;
+    boolean pict5 = false;
+    boolean pict6 = false;
+    boolean pict7 = false;
+    boolean pict8 = false;
+    boolean pict9 = false;
+
+
+
 }
